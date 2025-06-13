e825eb435bd850bfae7cc450cbe3d5a55d0d6887
Ewayrton Oliveira <164266609+Ewayrton@users.noreply.github.com>
2025-06-13 13:50:48 -0300

    Update CorsConfig.java

--- backend/springbootApp/src/main/java/com/example/demo/config/CorsConfig.java
+++ backend/springbootApp/src/main/java/com/example/demo/config/CorsConfig.java
@@ -6,8 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class
-CorsConfig {
+public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -15,7 +14,12 @@ CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // aplica para todos os endpoints
-                        .allowedOrigins("http://localhost:3000") // endereço do seu frontend
+                        // Agora permitimos múltiplas origens
+                        .allowedOrigins(
+                                "https://recifeirinha-front.vercel.app",   // Frontend na Vercel
+                                "https://teste2-deploy-render.onrender.com",// Backend no Render
+                                "http://localhost:3000"                      // Acesso para desenvolvimento local
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
